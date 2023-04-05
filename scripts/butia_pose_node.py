9a98c0e1bef98a1a56d295baecd86f9be529af2c
PedroCorcaque <pedrollcorc@gmail.com>
2023-04-05 12:19:02 -0400

    tests

--- scripts/butia_pose_node.py
+++ scripts/butia_pose_node.py
@@ -4,7 +4,14 @@ import ros_numpy
 
 from jetson_inference import poseNet
 from jetson_utils import cudaFromNumpy
-from butia_vision_bridge import VisionSynchronizer
+
+from butia_vision_bridge import VisionSynchronizer, VisionBridge
+from butia_vision_msgs.msg import Frame, BodyPart, Pixel, Person
+
+from sensor_msgs.msg import PointCloud2
+from geometry_msgs.msg import Point
+
+import copy
 
 class ButiaPose():
     
@@ -14,13 +21,13 @@ class ButiaPose():
 
         self._readParameters()
         self._loadNetwork()
-        VisionSynchronizer.syncSubscribers(self.source_topic_dict, self.callback)
 
+        VisionSynchronizer.syncSubscribers(self.source_topic_dict, self.callback)
 
         self.run()
         
     def run(self):
-        rate = rospy.Rate(10)
+        rate = rospy.Rate(1)
         while not rospy.is_shutdown():
             rate.sleep()
 
@@ -28,20 +35,51 @@ class ButiaPose():
         self.net = poseNet(self.network)
 
     def _readParameters(self):
-        self.source_topic_dict = {"image_rgb": "/camera/color/image_raw"}
+        self.source_topic_dict = {
+                "image_rgb": "/camera/color/image_raw",
+                "points": "/camera/depth/color/points",
+                }
 
         self.network = "resnet18-body"
 
     def callback(self, *args):
+
+        # Init the Frame message and write you header
+        frame = Frame()
+        frame.header.stamp = rospy.Time.now()
+
+        # Init the img and points variable
         img = None
+        points = None
+
+        # To verify if exists messages being published in topics (self.source_topic_dict)
         if len(args):
             img = args[0]
-        cv_img = ros_numpy.numpify(img)
-        cudaImage = cudaFromNumpy(cv_img)
+            points = args[1]
 
-        poses = self.net.Process(cudaImage, overlay="keypoints")
-        
-        print("Was detected {:d} people".format(len(poses)))
+        # Convert the message to numpy array
+        cv_img = ros_numpy.numpify(img)
+        cv_points = ros_numpy.numpify(points)
+
+        # Create cudaImage object from cv_img
+        cuda_image = cudaFromNumpy(cv_img)
+
+        # Get the poses (how many people) are being detected
+        poses = self.net.Process(cuda_image, overlay="keypoints")
+
+        # If any person was detected
+        if len(poses):
+            nb_persons = len(poses)
+            bodypart_count = len(poses)
+        else:
+            nb_persons = 0
+            bodypart_count = 0
+
+        # Create the Persons messages for each person detected
+        if nb_persons != 0:
+            print("Ola mundo---------------")
+            pc = ros_numpy.numpify(points)
+            print(len(pc.data))
 
 if __name__ == "__main__":
     rospy.init_node("pose_node")
